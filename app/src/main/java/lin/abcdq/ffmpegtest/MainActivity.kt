d865fd05916bcdbfe705845e82d37c317e2d3efa
wml <18981745898@163.com>
2021-06-30 18:07:51 +0800

    ffmpeg https 支持添加(README --enable-openssl \)

--- app/src/main/java/lin/abcdq/ffmpegtest/MainActivity.kt
+++ app/src/main/java/lin/abcdq/ffmpegtest/MainActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.opengl.GLSurfaceView
 import android.os.Bundle
 import android.os.Environment
+import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import lin.abcdq.ffmpeg.FFPlayer
@@ -11,8 +12,7 @@ import lin.abcdq.ffmpeg.FFPlayer
 class MainActivity : AppCompatActivity() {
 
     val yuv420p = "http://vfx.mtime.cn/Video/2019/03/21/mp4/190321153853126488.mp4"
-    private val mVideoPath =
-        Environment.getExternalStorageDirectory().absolutePath + "/byteflow/one_piece.mp4"
+    private var mVideoPath = ""
 
     private val mPlayer = FFPlayer()
     private lateinit var mButtonInfo: Button
@@ -24,7 +24,11 @@ class MainActivity : AppCompatActivity() {
         requestPermissions(mPermissions, 100)
         CAO.copyAssetsDirToSDCard(this, "video", this.obbDir.absolutePath)
         mButtonInfo = findViewById(R.id.bt_info)
-        mButtonInfo.setOnClickListener { mPlayer.onSource(mVideoPath) }
+        mButtonInfo.setOnClickListener {
+            mVideoPath = this.obbDir.absolutePath + "/video/one_piece.mp4"
+            Log.e("videoPath", "$mVideoPath")
+            mPlayer.onSource(mVideoPath)
+        }
         mGLSurfaceView = findViewById(R.id.gls_content)
         mGLSurfaceView.setEGLContextClientVersion(3)
         mGLSurfaceView.setRenderer(mPlayer)
